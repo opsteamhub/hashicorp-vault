f238fcb7756548dc850723776dd963014efed0ee
rafael <rafaelviniciuscardoso@gmail.com>
2022-03-10 14:55:31 -0300

    add provider 4.2.0

--- provider.tf
+++ provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.2.0"
+    }
+  }
+}
+
 provider "aws" {
   region = local.region_principal
 }
