37e7dc16f88cf26208fdf5ba2b37c4dfeb3c402e
Rafael Vinicius Cardoso Julio <rafael@macbook.local>
2023-07-28 16:19:57 -0300

    pass filebase64

--- autoscaling.tf
+++ autoscaling.tf
@@ -1,5 +1,5 @@
 locals {
-  user_data_vault = filebase64(
+  user_data_vault = templatefile(
     ".terraform/modules/hashicorp-vault/templates/user_data.tpl",
     {
       cluster_vault = base64encode(local.vault_name)
