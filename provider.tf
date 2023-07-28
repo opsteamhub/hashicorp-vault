efc3ba9fece568cb3adbb5a0a04e7a8973ae3c7b
Rafael Vinicius Cardoso Julio <rafael@macbook.local>
2023-07-28 16:04:14 -0300

    remove experiments

--- provider.tf
+++ provider.tf
@@ -16,9 +16,3 @@ provider "aws" {
   region = var.create_replica == true ? var.region_replica : var.region_principal
 
 }
-
-terraform {
-  # Optional attributes and the defaults function are
-  # both experimental, so we must opt in to the experiment.
-  experiments = [module_variable_optional_attrs]
-}
