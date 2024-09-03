0a79571eadfb85a4552cbd80e5d07bcdb13af855
faelvinicius <rafaelviniciuscardoso@gmail.com>
2024-09-03 17:44:51 -0300

    fix sg rule

--- aws_nat_instance.tf
+++ aws_nat_instance.tf
@@ -33,6 +33,7 @@ resource "aws_security_group_rule" "outbound-nat-instance" {
   to_port           = 0
   protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]
   security_group_id = aws_security_group.sg-nat-instance[0].id
 }
 
