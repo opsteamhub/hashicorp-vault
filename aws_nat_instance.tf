5577d883a9477f6292dfa84e00589f2f8f3edf41
faelvinicius <rafaelviniciuscardoso@gmail.com>
2024-09-03 16:31:36 -0300

    fix ebs

--- aws_nat_instance.tf
+++ aws_nat_instance.tf
@@ -63,7 +63,7 @@ resource "aws_instance" "nat-instance" {
 
   # Root disk for NAT instance 
   root_block_device {
-    volume_size = "2"
+    volume_size = "8"
     volume_type = "gp3"
     encrypted   = true
   }
