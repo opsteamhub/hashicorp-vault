3508aaa0d0c45cdc0b0921ea68cad4769e321752
faelvinicius <rafaelviniciuscardoso@gmail.com>
2024-09-04 10:55:50 -0300

    update 0

--- aws_nat_instance.tf
+++ aws_nat_instance.tf
@@ -15,7 +15,7 @@ resource "aws_security_group" "sg-nat-instance" {
 
 # NAT Instance security group rule to allow all traffic from within the VPC
 resource "aws_security_group_rule" "vpc-inbound" {
-  count             = var.create_nat_instance ? 1 : 0
+  count       = var.create_nat_instance ? 1 : 0
   type              = "ingress"
   from_port         = 0
   to_port           = 0
@@ -27,7 +27,7 @@ resource "aws_security_group_rule" "vpc-inbound" {
 
 # NAT Instance security group rule to allow outbound traffic
 resource "aws_security_group_rule" "outbound-nat-instance" {
-  count             = var.create_nat_instance ? 1 : 0
+  count       = var.create_nat_instance ? 1 : 0
   type              = "egress"
   from_port         = 0
   to_port           = 0
@@ -45,7 +45,7 @@ data "aws_ami" "natinstance_ami" {
   filter {
     name   = "name"
     values = ["amzn-ami-vpc-nat*"]
-  }
+  } 
 }
 
 ##ASG
@@ -60,11 +60,6 @@ resource "aws_launch_template" "nat_instance" {
   instance_initiated_shutdown_behavior = "terminate"
 
   instance_type = "t3.medium"
-
-  network_interfaces {
-    network_interface_id = aws_network_interface.nat_instance_network_interface[0].id
-    device_index         = 0
-  }
 
   monitoring {
     enabled = false
@@ -82,12 +77,12 @@ resource "aws_launch_template" "nat_instance" {
 }
 
 resource "aws_autoscaling_group" "nat_asg_vault" {
-  count               = var.create_nat_instance ? 1 : 0
-  name_prefix         = join("-", ["asg", "nat-instance", local.vault_name])
-  vpc_zone_identifier = [aws_subnet.pub_subnet_a_principal[0].id]
-  desired_capacity    = 1
-  min_size            = 1
-  max_size            = 1
+  count       = var.create_nat_instance ? 1 : 0
+  name_prefix                = join("-", ["asg", "nat-instance", local.vault_name])
+  vpc_zone_identifier = [aws_subnet.pub_subnet_a_principal[0].id, aws_subnet.pub_subnet_b_principal[0].id]
+  desired_capacity          = 1
+  min_size                  = 1
+  max_size                  = 1
 
   launch_template {
     id      = aws_launch_template.nat_instance[0].id
@@ -145,37 +140,18 @@ resource "aws_eip" "nat_instance_eip" {
   vpc   = true
 
   tags = {
-    "Name"          = join("-", ["eip", "nat-instance", local.vault_name])
-    "ProvisionedBy" = local.provisioner
-    "Squad"         = local.squad
-    "Service"       = local.service
+    "Name" = join("-", ["eip", "nat-instance", local.vault_name])
   }
 }
 
 resource "aws_eip_association" "nat_instance_eip_assoc" {
-  count                = var.create_nat_instance && length(aws_network_interface.nat_instance_network_interface.*.id) > 0 ? 1 : 0
-  network_interface_id = aws_network_interface.nat_instance_network_interface[0].id
-  allocation_id        = aws_eip.nat_instance_eip[0].id
-}
-
-resource "aws_network_interface" "nat_instance_network_interface" {
-  count             = var.create_nat_instance ? 1 : 0
-  subnet_id         = aws_subnet.pub_subnet_a_principal[0].id
-  security_groups   = [aws_security_group.sg-nat-instance[0].id]
-  source_dest_check = false
-
-  tags = {
-    "Name"          = join("-", ["eni", "nat-instance", local.vault_name])
-    "ProvisionedBy" = local.provisioner
-    "Squad"         = local.squad
-    "Service"       = local.service
-  }
-}
-
-resource "aws_network_interface_attachment" "nat_instance_attachment" {
   count = var.create_nat_instance && length(data.aws_instances.nat_instance.ids) > 0 ? 1 : 0
 
-  instance_id          = data.aws_instances.nat_instance.ids[0]
-  network_interface_id = aws_network_interface.nat_instance_network_interface[0].id
-  device_index         = 0 # Você pode alterar este valor conforme necessário
+  instance_id   = data.aws_instances.nat_instance.ids[0]
+  allocation_id = aws_eip.nat_instance_eip[0].id
+
+  depends_on = [
+    aws_autoscaling_group.nat_asg_vault,
+    aws_eip.nat_instance_eip
+  ]
 }
