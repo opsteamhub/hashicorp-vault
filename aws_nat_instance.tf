eff68e7c327785a99d98e25dd1772d6e48c9a9af
faelvinicius <rafaelviniciuscardoso@gmail.com>
2024-09-04 14:47:10 -0300

    update provide region

--- aws_nat_instance.tf
+++ aws_nat_instance.tf
@@ -186,6 +186,7 @@ resource "aws_network_interface_attachment" "nat_instance_attachment" {
 # Security Group for NAT Instance
 resource "aws_security_group" "sg-nat-instance_replica" {
   count       = var.create_nat_instance && var.create_replica ? 1 : 0
+  provider = aws.replica
   name        = join("-", ["SG", "nat-instance", local.vault_name])
   description = "Security Group for NAT instance"
   vpc_id      = var.create_vpc == "false" ? var.vpc_id : aws_vpc.vpc[0].id
@@ -201,6 +202,7 @@ resource "aws_security_group" "sg-nat-instance_replica" {
 # NAT Instance security group rule to allow all traffic from within the VPC
 resource "aws_security_group_rule" "vpc-inbound_replica" {
   count             = var.create_nat_instance && var.create_replica ? 1 : 0
+  provider = aws.replica
   type              = "ingress"
   from_port         = 0
   to_port           = 0
@@ -213,6 +215,7 @@ resource "aws_security_group_rule" "vpc-inbound_replica" {
 # NAT Instance security group rule to allow outbound traffic
 resource "aws_security_group_rule" "outbound-nat-instance_replica" {
   count             = var.create_nat_instance && var.create_replica ? 1 : 0
+  provider = aws.replica
   type              = "egress"
   from_port         = 0
   to_port           = 0
@@ -224,6 +227,7 @@ resource "aws_security_group_rule" "outbound-nat-instance_replica" {
 
 
 data "aws_ami" "natinstance_ami_replica" {
+  provider = aws.replica
   most_recent = true
   owners      = ["amazon"]
 
@@ -236,6 +240,7 @@ data "aws_ami" "natinstance_ami_replica" {
 ##ASG
 resource "aws_launch_template" "nat_instance_replica" {
   count       = var.create_nat_instance && var.create_replica ? 1 : 0
+  provider = aws.replica
   name_prefix = join("-", ["lt", "nat", local.vault_name])
 
   image_id = data.aws_ami.natinstance_ami_replica.id
@@ -265,6 +270,7 @@ resource "aws_launch_template" "nat_instance_replica" {
 
 resource "aws_autoscaling_group" "nat_asg_vault_replica" {
   count       = var.create_nat_instance && var.create_replica ? 1 : 0
+  provider = aws.replica
   name_prefix = join("-", ["asg", "nat-instance", local.vault_name])
   #vpc_zone_identifier = [aws_subnet.pub_subnet_a_principal[0].id]
   availability_zones = ["us-east-1a"]
@@ -307,6 +313,7 @@ resource "aws_autoscaling_group" "nat_asg_vault_replica" {
 }
 
 data "aws_instances" "nat_instance_replica" {
+  provider = aws.replica
   filter {
     name   = "tag:Name"
     values = [join("-", ["asg", "nat-instance", local.vault_name])]
@@ -320,6 +327,7 @@ data "aws_instances" "nat_instance_replica" {
 
 resource "aws_network_interface" "nat_instance_network_interface_replica" {
   count       = var.create_nat_instance && var.create_replica ? 1 : 0
+  provider = aws.replica
   subnet_id         = aws_subnet.pub_subnet_a_replica[0].id
   security_groups   = [aws_security_group.sg-nat-instance_replica[0].id]
   source_dest_check = false
@@ -334,6 +342,7 @@ resource "aws_network_interface" "nat_instance_network_interface_replica" {
 
 resource "aws_eip" "nat_instance_eip_replica" {
   count       = var.create_nat_instance && var.create_replica ? 1 : 0
+  provider = aws.replica
   vpc   = true
 
   tags = {
@@ -346,13 +355,15 @@ resource "aws_eip" "nat_instance_eip_replica" {
 
 resource "aws_eip_association" "nat_instance_eip_assoc_replica" {
   count                = var.create_nat_instance && var.create_replica && length(aws_network_interface.nat_instance_network_interface_replica.*.id) > 0 ? 1 : 0
+  provider = aws.replica
   network_interface_id = aws_network_interface.nat_instance_network_interface_replica[0].id
   allocation_id        = aws_eip.nat_instance_eip_replica[0].id
 }
 
 resource "aws_network_interface_attachment" "nat_instance_attachment_replica" {
   count = var.create_nat_instance && var.create_replica && length(data.aws_instances.nat_instance_replica.ids) > 0 ? 1 : 0
-
+  provider = aws.replica
+  
   instance_id          = data.aws_instances.nat_instance_replica.ids[0]
   network_interface_id = aws_network_interface.nat_instance_network_interface_replica[0].id
   device_index         = 0 # Você pode alterar este valor conforme necessário
