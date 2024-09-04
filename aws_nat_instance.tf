c7a3cdf5188c176dd0644c4526787c8622f31a6d
faelvinicius <rafaelviniciuscardoso@gmail.com>
2024-09-04 12:28:12 -0300

    upate az

--- aws_nat_instance.tf
+++ aws_nat_instance.tf
@@ -63,6 +63,7 @@ resource "aws_launch_template" "nat_instance" {
 
   network_interfaces {
     network_interface_id = aws_network_interface.nat_instance_network_interface[0].id
+    security_groups   = [aws_security_group.sg-nat-instance[0].id]
     device_index         = 0
   }
 
