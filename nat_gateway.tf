8aeed4b7036b0ef53463bcbe64b70de6c83b597d
faelvinicius <rafaelviniciuscardoso@gmail.com>
2024-09-16 15:28:24 -0300

    fix conditional in route 2

--- nat_gateway.tf
+++ nat_gateway.tf
@@ -14,7 +14,7 @@ resource "aws_nat_gateway" "nat_gateway_pub_a_principal" {
 #####
 
 resource "aws_nat_gateway" "nat_gateway_pub_a_replica" {
-  count         = var.create_replica ? 0 : 1
+  count = (var.create_nat_instance ? 0 : 1) * (var.create_replica ? 1 : 0)
   provider      = aws.replica
   allocation_id = aws_eip.eip_replica[0].id
   subnet_id     = aws_subnet.pub_subnet_a_replica[0].id
